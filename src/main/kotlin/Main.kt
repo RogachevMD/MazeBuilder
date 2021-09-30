36dbeb76c4230cd587b33164d5a652d116d7386f
TOPSTREJIOK <misha1997mtl@gmail.com>
2021-09-30 15:48:20 +0400

    1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+var sizeX: Int = 11
+var sizeY: Int = 11
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+
+fun main(args: Array<String>) {
+    init(args)
+
+
+}
+
+fun init(args: Array<String>) {
+    if (args.size != 0) {
+        for (i in args.indices) {
+            println("arg$i: ${args[i]}")
+            sizeX = args[i].toInt()
+
+        }
+    }
+
+    if (args.size == 0) {
+        println("No args")
+    } else {
+        for (i in args.indices){
+
+        }
+    }
 }
