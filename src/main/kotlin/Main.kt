55c19040df3ad70f27cec02087a14e5d161585ed
TOPSTREJIOK <misha1997mtl@gmail.com>
2021-09-30 22:06:24 +0400

    3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val maze: Maze = Maze(11, 11)
+    val maze = Maze(11, 11)
 }
 
 fun init(args: Array<String>) {
--- src/main/kotlin/Maze.kt
+++ src/main/kotlin/Maze.kt
@@ -1,9 +1,10 @@
 class Maze(sizeX: Int = 5, sizeY: Int = 5) {
 
-    var maze: Array<Array> = Array(1) { Array(1) };
+    var maze: Array<IntArray> = Array(1) { IntArray(1) };
 
     init {
         init(sizeX, sizeY)
+        generateMaze(sizeX, sizeY)
     }
 
     fun init(x: Int, y: Int) {
@@ -11,8 +12,20 @@ class Maze(sizeX: Int = 5, sizeY: Int = 5) {
         println(maze.size)
     }
 
-    fun generateMaze() {
-
+    fun generateMaze(x: Int, y: Int) {
+        for (rangeY in 0..y - 1) {
+            for (rangeX in 0..x - 1) {
+                print("[$rangeX $rangeY]")
+                if ((rangeY % 2 != 0 && rangeY % 2 != 0) &&
+                    (rangeX < x - 1 && rangeY < y - 1)
+                ) {
+                    //maze[rangeX, rangeY] = 8
+                }else{
+                    //maze[rangeX, rangeY] = 0
+                }
+            }
+            println()
+        }
     }
 
     fun printMaze() {
